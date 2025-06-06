2183f33144a41336249570d5a44a66fa08c1a80d
poby123 <i2pir@naver.com>
2025-06-07 00:43:46 +0900

    Revert "fix: tla"
    This reverts commit 836ed4a5c3ad3973d92e6e6419929a38272a153c.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +10,6 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    topLevelAwait({
-      promiseExportName: '__tla',
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
   ],
   resolve: {
     alias: {
