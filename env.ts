72fd9883ca8d34faff187590be3bd76f31633bae
poby123 <i2pir@naver.com>
2025-06-06 15:15:40 +0900

    feat: deploy script

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://ec2-3-39-232-42.ap-northeast-2.compute.amazonaws.com:8001';
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'http://ec2-3-39-232-42.ap-northeast-2.compute.amazonaws.com:8001';
+// export const BASE_URL = 'http://localhost:3001';
